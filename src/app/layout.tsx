4886a6cb1ce2ed3474d8e55d10fe3e1a2ea11e49
NotJerwee <jerrylin2488@gmail.com>
2025-05-29 23:22:50 -0400

    chore(analytics): install and integrate Vercel Web Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import "./globals.css";
 import type { Metadata } from "next";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
+import { Analytics } from "@vercel/analytics/next"
 
 export const metadata: Metadata = {
 	title: "SBU Ranks",
@@ -19,6 +20,7 @@ export default function RootLayout({
 				<Navbar />
 				<main className="flex-grow">{children}</main>
 				<Footer />
+				<Analytics />
 			</body>
 		</html>
 	);
